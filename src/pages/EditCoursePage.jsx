e15f54ca059b2b12144b4509bea5a224096c6c8b
anuzsubedi <anuzsubedi@hotmail.com>
2024-11-27 04:32:44 -0500

    Add placeholder EditCoursePage component with a coming soon message

---
+++ src/pages/EditCoursePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
+
+const EditCoursePage = () => (
+    <Box textAlign="center" py={10}>
+        <Heading as="h1" size="lg">
+            Edit Course (Coming Soon!)
+        </Heading>
+    </Box>
+);
+
+export default EditCoursePage;
