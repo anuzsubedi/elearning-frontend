302e3fc18d8beee432425cf72e53c6b9251c6d6b
anuzsubedi <anuzsubedi@hotmail.com>
2024-12-05 00:51:30 -0500

    Refactor Header component to include navigation links and wrap in Box for improved layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,9 @@ import {
     MenuList,
     MenuItem,
     Spacer,
+    Box,
+    HStack,
+    Link,
 } from "@chakra-ui/react";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
@@ -22,44 +25,62 @@ const Header = () => {
     };
 
     return (
-        <Flex
-
-            px={6}
-            py={4}
-            top={0}
-            left={0}
-            right={0}
-            maxW="80%"
-            mx="auto"
-            mb={4}
-            align="center"
-        >
-            <Text fontSize="3xl" fontWeight="bold" color="brand.primary">
-                Next<span style={{ color: "black" }}>Academy</span>
-            </Text>
-            <Spacer />
-            {
-                user ? (
-                    <Menu>
-                        <MenuButton>
-                            <Avatar size="sm" name={user.full_name} />
-                        </MenuButton>
-                        <MenuList>
-                            <MenuItem>My Details</MenuItem>
-                            <MenuItem>My Courses</MenuItem>
-                            <MenuItem onClick={handleLogout}>Logout</MenuItem>
-                        </MenuList>
-                    </Menu>
-                ) : (
-                    <Button
-                        colorScheme="orange"
-                        onClick={() => (window.location.href = "/login")}
-                    >
-                        Login
-                    </Button>
-                )
-            }
-        </Flex >
+        <Box p={4}>
+            <Flex
+                px={6}
+                py={4}
+                top={0}
+                left={0}
+                right={0}
+                maxW="80%"
+                mx="auto"
+                mb={4}
+                align="center"
+            >
+                <Text fontSize="3xl" fontWeight="bold" color="brand.primary">
+                    Next<span style={{ color: "black" }}>Academy</span>
+                </Text>
+                <Spacer />
+                {
+                    user ? (
+                        <Menu>
+                            <MenuButton>
+                                <Avatar size="sm" name={user.full_name} />
+                            </MenuButton>
+                            <MenuList>
+                                <MenuItem>My Details</MenuItem>
+                                <MenuItem>My Courses</MenuItem>
+                                <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                            </MenuList>
+                        </Menu>
+                    ) : (
+                        <Button
+                            colorScheme="orange"
+                            onClick={() => (window.location.href = "/login")}
+                        >
+                            Login
+                        </Button>
+                    )
+                }
+            </Flex>
+            <Flex
+                px={6}
+                py={2}
+                top={0}
+                left={0}
+                right={0}
+                maxW="70%"
+                mx="auto"
+                mb={4}
+                align="center"
+                justify="space-between"
+            >
+                <Link href="/" p={2}>Home</Link>
+                <Link href="/my-courses" p={2}>My Courses</Link>
+                <Link href="/top-rated" p={2}>Top Rated</Link>
+                <Link href="/my-profile" p={2}>My Profile</Link>
+            </Flex>
+        </Box>
     );
 };
 
